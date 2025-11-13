944916a0dc4ab784ead987d629892840ed958c25
AnamXI <anamggss1227@gmail.com>
2025-11-13 17:39:09 +0800

    added logo

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Navbar.css';
+import logo from "./img/logo.png"
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="nav-links">
         <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about"><img src={logo} alt='logo' className='logo'/></a></li>
+        <li><a href="#contact">Profile</a></li>
       </ul>
     </nav>
   );
